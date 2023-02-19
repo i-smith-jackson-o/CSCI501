93c150b6a587f54581f3b458ecc91590838b256c
i-smith-jackson-o <i.smith.jackson.o@gmail.com>
2023-02-19 01:40:23 -0500

    foo

---
+++ chapter_2/program2-18.cpp
@@ -0,0 +1,24 @@
+// This program determines the size of integers, long integers
+// and long doubles. 
+#include <iostream>
+using namespace std; 
+
+int main() {
+  long double apple; 
+
+  cout << "int " << sizeof(int) << " bytes\n"; 
+  cout << "long int" << sizeof(long) << " bytes\n"; 
+  cout <<"long double" << sizeof(apple) << " bytes"; 
+
+  return 0; 
+}
+
+
+
+
+
+
+
+
+
+
