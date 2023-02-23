1bbf0c2b7ac1f81a326e8541371934314049d791
i-smith-jackson-o <i.smith.jackson.o@gmail.com>
2023-02-23 12:46:03 -0500

    added random number generator for sort algo tests

---
+++ assignment_1/intGen.cpp
@@ -0,0 +1,26 @@
+// Randomly generate 1000 integers between 1 - 10000
+#include <iostream>
+#include <random>
+#include <fstream>
+#include <vector>
+
+int main() {
+  std::mt19937 mt{ std::random_device{}() }; 
+  std::uniform_int_distribution<> tenK {0,10000}; 
+  std::ofstream outputFile; 
+  std::vector<int> numbers {}; 
+
+  for (int i = 0; i < 999; i++) {
+    int temp = tenK(mt); 
+    numbers.push_back(temp); 
+  }
+
+  outputFile.open("1kIntegers.txt");
+
+  for (int i = 0; i < numbers.size() - 1; i++) 
+    outputFile << numbers[i] << std::endl; 
+
+  outputFile.close(); 
+  std::cout << "Done" << std::endl; 
+  return 0; 
+}
