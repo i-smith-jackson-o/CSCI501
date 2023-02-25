e3975ab92e18555dff418bd19c3ba4f377f1484c
i-smith-jackson-o <i.smith.jackson.o@gmail.com>
2023-02-24 21:03:01 -0500

    moving along

--- chapter_8/linear.cpp
+++ chapter_8/linear.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
 
 int main() {
-  std::vector<int> arr = {12, 15, 57, 24, 75, 24, 64, 13, 65, 43}; 
+  int temp; 
+  std::ifstream inputFile; 
+  std::vector<int> arr = {}; 
   bool found = false; 
   int position = -1; 
   int index = 0; 
-  int searchValue = 99; 
+  int searchValue = 6394; 
+
+  inputFile.open("1kIntegers.txt"); 
+  for (int i = 0; i < 1000; i++) {
+    inputFile >> temp; 
+    arr.push_back(temp); 
+  }
+  inputFile.close(); 
 
   while (!found && index < arr.size()) {
     if (arr[index] == searchValue) {
@@ -15,6 +25,7 @@ int main() {
     }
     index++; 
   }
+
   std::cout << position << std::endl; 
   return 0; 
 }
