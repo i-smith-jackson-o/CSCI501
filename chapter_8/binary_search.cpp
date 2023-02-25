e8ac0e4caa42406beeb91e221f177756a54e1404
i-smith-jackson-o <i.smith.jackson.o@gmail.com>
2023-02-24 20:50:56 -0500

    completed binary-search and added program8-2

--- chapter_8/binary_search.cpp
+++ chapter_8/binary_search.cpp
@@ -1,50 +1,66 @@
 #include <iostream>
 #include <vector>
-const int SIZE = 10; 
 
-int binarySearchArray(const int arr[], int value) {
-  int index = 0; 
-  int position = -1; 
-  int lastIndex = SIZE - 1; 
+int binarySearchArray(const int arr[], int size, int value) {
+  int idx = 0, 
+      last = size - 1, 
+      mid, 
+      pos = -1; 
   bool found = false; 
 
-  while(!found && index <= lastIndex) {
-    int middle = lastIndex / 2; 
-    if (value == arr[middle]) {
+  while (!found && idx <= last) {
+    mid = (idx+ last) / 2; 
+    if (arr[mid] == value) {
       found = true; 
-      position = middle; 
-    } else if (arr[middle] > value) {
-      lastIndex = middle - 1; 
-    } else {
-      lastIndex = middle + 1; 
+      pos = mid; 
     }
+    else if (arr[mid] > value)
+      last = mid - 1; 
+    else 
+      idx = mid + 1; 
   }
-  return position; 
+  return pos; 
 }
 
 int binarySearchVector(std::vector<int>& vec, int value) {
+  int idx = 0, 
+      last = vec.size() - 1, 
+      mid, 
+      pos = -1; 
+  bool found = false; 
 
-  return 0; 
+  while (!found && idx <= last) {
+    mid = (idx+ last) / 2; 
+    if (vec[mid] == value) {
+      found = true; 
+      pos = mid; 
+    }
+    else if (vec[mid] > value)
+      last = mid - 1; 
+    else 
+      idx = mid + 1; 
+  }
+  return pos; 
 }
 
 void printResult(int result, int value) {
   if (result == -1)
     std::cout << value << " not found.\n"; 
   else {
-    std::cout << value << " found at index "(result + 1) << "." << std::endl; 
+    std::cout << value << " found at index " << result  << "." << std::endl; 
   }
 }
 
 int main() {
-  // Values must first be sorted. 
+  const int SIZE = 10; 
   int testArr[SIZE] = {12, 15, 23, 26, 31, 36, 42, 46, 48, 53}; 
   std::vector<int> testVec{12, 15, 23, 26, 31, 36, 42, 46, 48, 53}; 
   int value = 42; 
+  
+  int arrResult = binarySearchArray(testArr, SIZE, value); 
+  int vecResult = binarySearchVector(testVec, value); 
 
-  int arrRes = binarySearchArray(testArr, value); 
-//  int vecRes = binarySearchVector(testVec, value); 
-
-  printResult(arrRes, value); 
-  printResult(vecRes, value); 
+  printResult(arrResult, value); 
+  printResult(vecResult, value); 
   return 0; 
 }
