77ec788043022147672ed6cd0682d29e6c41d916
i-smith-jackson-o <i.smith.jackson.o@gmail.com>
2023-03-11 23:06:22 -0500

    starting binaryStringSearch

---
+++ chapter_8/challenges/binaryStringSearch.cpp
@@ -0,0 +1,4 @@
+//  Modify the binarySearch function so it searchs an array of strings 
+//  instead of an array of ints. Test the function with a driver program. 
+//  use program 8-8 as a skeleton to complete. the array must be sorted 
+//  before the binary search will work. 
